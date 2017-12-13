6c1a5ad277030e380c794e9dca25408b15ccf340
RABBAH Mahmoud <mrabbah@gmail.com>
2017-12-13 00:48:33 +0000

    Saving progress

--- copdai_core/mas.py
+++ copdai_core/mas.py
@@ -8,7 +8,8 @@ import logging
 from logging.handlers import RotatingFileHandler
 from logging import handlers
 from pathlib import Path
-from uuid import getnode
+from uuid import getnode, uuid4
+from abc import ABC, abstractmethod
 
 # Setting logging system for development environment
 home = str(Path.home())
@@ -21,13 +22,12 @@ format = logging.Formatter('%(asctime)s.%(msecs)03d %(levelname)s:(%(threadName)
 sh = logging.StreamHandler(sys.stdout)
 sh.setFormatter(format)
 log.addHandler(sh)
-fh = handlers.RotatingFileHandler("%s%scopdai_core.log" % (home, os.pathsep), maxBytes=(1048576*5), backupCount=7)
+fh = handlers.RotatingFileHandler("%s%scopdai_core.log" % (home, os.pathsep), maxBytes=(1048576 * 5), backupCount=7)
 fh.setFormatter(format)
 log.addHandler(fh)
 
 @unique
 class AgentState(Enum):
-
     """Normal agent life cycle FIPA specification"""
 
     UNKNOWN = 0
@@ -37,7 +37,8 @@ class AgentState(Enum):
     WAITING = 4
     TRANSIT = 5
 
-class Agent(object):
+
+class AbstractAgent(ABC):
     """
     An Agent is the fundamental actor on an AP which
     combines one or more service capabilities into a
@@ -48,43 +49,74 @@ class Agent(object):
     for accessing software (see [FIPA00079]).
     """
 
-    def __init__(self, name, id):
-        self.state = AgentState.UNKNOWN
-        self.name = name
-        self.id = id
+    __slots__ = ['_state', '_platform_id', '_pid', '_aid', '_run']
+
+    def __init__(self, platform_id=None, name=None):
+        super().__init__()
+        # The platform ID in normal case will be the MAC address of current machine
+        if platform_id is None:
+            self._platform_id = getnode()
+        else:
+            self._platform_id = platform_id
+
+        self._pid = os.getpid()
+        self._run = False
+        # A globally unique name for the agent
+        # the name will be composed from an ID + filename + platform ID
+        if name is None:
+            self._aid = '%s#%s@%s' % (
+                str(uuid4()),
+                __name__,
+                ''.join(("%012X" % self._platform_id)[i:i + 2] for i in range(0, 12, 2))
+            )
+        else:
+            self.name = '%s#%s@%s' % (
+                name,
+                __name__,
+                ''.join(("%012X" % self._platform_id)[i:i + 2] for i in range(0, 12, 2))
+            )
+
+        # register to handle TERM signal
+        # The graceful termination of an agent. This can be ignored by the agent.
+        signal.signal(signal.SIGTERM, self.signal_term)
+        # register to handle KILL signal
+        # The forceful termination of an agent. This can only be initiated by the AMS and cannot be ignored by the agent
+        signal.signal(signal.SIGKILL, self.signal_kill)
+        # register to handle STOP signal
+        # Puts an agent in a suspended state. This can be initiated by the agent or the AMS.
+        signal.signal(signal.SIGSTOP, self.signal_stop)
+        # register to handle CONT signal
+        # Brings the agent from a suspended state. This can only be initiated by the AMS.
+        signal.signal(signal.SIGCONT, self.signal_cont)
         # register to handle USR1 signal
+        # Brings the agent from a waiting state. This can only be initiated by the AMS.
         signal.signal(signal.SIGUSR1, self.signal_usr1)
 
+        self._state = AgentState.INITIATED
 
-    def invoke(self):
-        log.debug("Agent go to active state")
-        self.state = AgentState.ACTIVE
-        return ReturnCodes.SUCCESS
+    def continue_handling_signal(self, target_agent_state):
+        if target_agent_state == self._state:
+            log.debug('Agent already in %s state' % target_agent_state.name)
+            return False
+        elif target_agent_state == AgentState.SUSPENDED and self._state == AgentState.ACTIVE:
+            return True
+        elif target_agent_state == AgentState.WAITING and self._state == AgentState.ACTIVE:
+            return True
+        elif target_agent_state == AgentState.TRANSIT and self._state == AgentState.ACTIVE:
+            return True
+        elif target_agent_state == AgentState.ACTIVE and (self._state == AgentState.WAITING or self._state == AgentState.SUSPENDED or self._state == AgentState.INITIATED or self._state == AgentState.TRANSIT):
+            return True
+        log.debug('Cannot go to %s state, current state is : %s' % (target_agent_state.name, self._state.name))
+        return False
 
-    def suspend(self):
-        log.debug("Agent go to suspend state")
-        self.state = AgentState.SUSPENDED
-        return ReturnCodes.SUCCESS
+    @abstractmethod
+    def run(self):
+        log.debug('starting execution of agent ...')
 
-    def wait(self):
-        log.debug("Agent go to wait state")
-        self.state = AgentState.WAITING
-        return ReturnCodes.SUCCESS
-
-    def wakeup(self):
-        log.debug("Agent go to active state")
-        self.state = AgentState.ACTIVE
-        return ReturnCodes.SUCCESS
-
-    def move(self):
-        log.debug("Agent go to transit state")
-        self.state = AgentState.TRANSIT
-        return ReturnCodes.SUCCESS
-
-    def execute(self):
-        log.debug("Agent go to active state")
-        self.state = AgentState.ACTIVE
-        return ReturnCodes.SUCCESS
+    def _start(self):
+        if self._run :
+            self.run()
+        signal.pause()
 
     def signal_usr1(self, signum, frame):
         """
@@ -93,8 +125,114 @@ class Agent(object):
         :return:
         """
         log.debug("Agent Received USR1 signal from")
+        self.wakeup()
 
-    #TODO state destroy and create
+    def signal_term(self, signum, frame):
+        log.debug("receiving signal TERM")
+        self.quit()
+
+    def signal_kill(self, signum, frame):
+        log.debug("receiving signal KILL")
+        self.destroy()
+
+    def signal_stop(self, signum, frame):
+        log.debug("receiving signal STOP")
+        if self.continue_handling_signal(AgentState.SUSPENDED):
+            self.suspend()
+
+    def signal_cont(self, signum, frame):
+        log.debug("receiving signal CONT")
+        if self.continue_handling_signal(AgentState.ACTIVE):
+            self.resume()
+
+
+    def resume(self):
+        """
+        Brings the agent from a suspended state. This can only be initiated by the AMS.
+        :return:
+        """
+        log.debug("Resuming agent")
+        self._state = AgentState.ACTIVE
+        return ReturnCodes.SUCCESS
+
+    def invoke(self):
+        """
+        The invocation of a new agent.
+        :return:
+        """
+        log.debug("Agent go to active state")
+        self._state = AgentState.ACTIVE
+        return ReturnCodes.SUCCESS
+
+    def suspend(self):
+        """
+        Puts an agent in a suspended state. This can be initiated by the agent or the AMS.
+        :return:
+        """
+        log.debug("Agent go to suspend state")
+        self._state = AgentState.SUSPENDED
+        return ReturnCodes.SUCCESS
+
+    def wait(self):
+        """
+        Puts an agent in a waiting state. This can only be initiated by an agent.
+        :return:
+        """
+        if self.continue_handling_signal(AgentState.WAITING):
+            log.debug("Agent go to wait state")
+            self._state = AgentState.WAITING
+            self._run = False
+
+        return ReturnCodes.SUCCESS
+
+    def wakeup(self):
+        """
+        Brings the agent from a waiting state. This can only be initiated by the AMS.
+        :return:
+        """
+        log.debug("Agent go to active state")
+        self._state = AgentState.ACTIVE
+        return ReturnCodes.SUCCESS
+
+    def move(self):
+        """
+        The following transition are only used by mobile agents:
+        Puts the agent in a transitory state. This can only be initiated by the agent.
+        :return:
+        """
+        log.debug("Agent go to transit state")
+        self._state = AgentState.TRANSIT
+        return ReturnCodes.SUCCESS
+
+    def execute(self):
+        """
+        The following transition are only used by mobile agents:
+        Brings the agent from a transitory state. This can only be initiated by the AMS.
+        :return:
+        """
+        log.debug("Agent go to active state")
+        self._state = AgentState.ACTIVE
+        return ReturnCodes.SUCCESS
+
+
+    def destroy(self):
+        """
+        The forceful termination of an agent. This can only be initiated by the AMS and cannot be ignored by the agent
+        :return:
+        """
+        log.debug("Agent Destroyed")
+        self._state = AgentState.UNKNOWN
+        return ReturnCodes.SUCCESS
+
+    def quit(self):
+        """
+        The graceful termination of an agent. This can be ignored by the agent.
+        :return:
+        """
+        log.debug("Agent Quit")
+        self._state = AgentState.UNKNOWN
+        return ReturnCodes.SUCCESS
+
 
 
 class AgentPlatform(object):
@@ -106,7 +244,8 @@ class AgentPlatform(object):
     def __init__(self, uid):
         self.uid = uid
 
-class AgentManagementSystem(object):
+
+class AgentManagementSystem(AbstractAgent):
     """
     An Agent Management System (AMS) is a mandatory component of
     the AP.
@@ -117,6 +256,13 @@ class AgentManagementSystem(object):
     addresses (amongst other things) for agents registered with the
     AP.
     """
+
+
+    def __init__(self):
+        # The platform ID in normal case will be the MAC address of current machine
+        super().__init__()
+        self._aid = "ams@%s" % ''.join(("%012X" % self._platform_id)[i:i + 2] for i in range(0, 12, 2))
+
 
     # PART1 :  Management Functions Supported by the Agent Management System
     def register(self):
@@ -132,32 +278,41 @@ class AgentManagementSystem(object):
         return ReturnCodes.SUCCESS
 
     def get_description(self):
-        return ReturnCodes.SUCCESS
+        """
+        An agent can make a query in order to request the platform profile of an AP from an AMS.
+        :return: platform ID
+        """
+        return ''.join(("%012X" % self._platform_id)[i:i + 2] for i in range(0, 12, 2))
 
     # END PART1
 
     # PART2 : AMS can instruct the underlying AP to perform the following operations
-    def invoke(self):
+    def invoke(self, aid):
         log.debug("Agent go to active state")
         return ReturnCodes.SUCCESS
 
-    def suspend(self):
+    def suspend(self, aid):
         log.debug("Agent go to suspend state")
         return ReturnCodes.SUCCESS
 
-    def terminate(self):
+    def terminate(self, aid):
         log.debug("Agent go to unkowen  state")
         return ReturnCodes.SUCCESS
 
-    def resume(self):
+    def resume(self, aid):
         log.debug("Agent go to active  state")
         return ReturnCodes.SUCCESS
 
     def create(self, agent_path_file_name):
+        """
+        The creation or installation of a new agent
+        :param agent_path_file_name:
+        :return:
+        """
         log.debug("Creating agent")
         return ReturnCodes.SUCCESS
 
-    def execute(self):
+    def execute(self, aid):
         log.debug("Agent go to active state")
         return ReturnCodes.SUCCESS
 
@@ -200,7 +355,6 @@ class MessageTransportService(object):
     """
 
     def deliverMessage(self, agent):
-
         """
         The MTS delivers messages to the agent as normal if it is in state
         active, if it is in state (Initiated/Waiting/Suspended) The MTS either
@@ -212,10 +366,10 @@ class MessageTransportService(object):
         return ReturnCodes.SUCCESS
 
     def bufferMessage(self):
-
         return ReturnCodes.SUCCESS
 
-# mac = get_mac()
-# sn = ''.join(("%012X" % mac)[i:i + 2] for i in range(0, 12, 2))
-
-#def run_command(command):
+"""
+One or more transport-descriptions, each of which is a self describing structure containing a transport-type, 
+a transport-specific-address and zero or more transport-specific-properties used to communicate with the agent
+#self.locator = locator
+"""
